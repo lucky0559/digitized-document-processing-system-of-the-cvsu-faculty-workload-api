7158a0145037a90b32c5289db32b471dd273b531
Lucky Angelo Rabosa <angelorabosa55@yahoo.com.ph>
2022-08-25 21:51:56 +0800

    set dynamic port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port: number = parseInt(`${process.env.PORT}`) || 3000;
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(port);
 }
 bootstrap();
