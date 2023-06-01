eb0ef4d3e05fe6520599db7e72ac8549f5954ba1
Lucky Angelo Rabosa <angelorabosa55@yahoo.com.ph>
2023-06-01 15:26:17 +0800

    fixed cors problem

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,8 @@ import { AppModule } from './app.module';
 import 'reflect-metadata';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  // app.enableCors({
-  //   origin: true,
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //   credentials: true,
-  // });
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
