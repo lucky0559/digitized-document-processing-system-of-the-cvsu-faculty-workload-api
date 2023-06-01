15827548e63ff1466199d30c7964e4556d02812e
Lucky Angelo Rabosa <angelorabosa55@yahoo.com.ph>
2023-06-01 16:43:35 +0800

    fixed cors problem

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,8 @@ import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const options = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-    allowedHeaders: 'Content-Type, Accept',
-  };
-  app.enableCors(options);
+
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
