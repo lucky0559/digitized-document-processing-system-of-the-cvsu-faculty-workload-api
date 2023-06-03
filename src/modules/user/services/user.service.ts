216c3c1b1b0436d4aaf2f5c9e60f056f917cc1fa
Lucky Angelo Rabosa <angelorabosa55@yahoo.com.ph>
2023-06-03 08:30:42 +0800

    added await on sendMail register

--- src/modules/user/services/user.service.ts
+++ src/modules/user/services/user.service.ts
@@ -74,7 +74,7 @@ export class UserService {
       }
     };
 
-    sendMail()
+    await sendMail()
       .then((result) => console.log('Email sent', result))
       .catch((error) => console.log(error.message));
   }
